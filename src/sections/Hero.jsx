a96de7838a66b02ca3653ebff058fd32651cf62c
abinjoseph2050 <abinjoseph2050@gmail.com>
2025-07-26 17:04:14 +0530

    Bug fixes and updtes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {words} from "../constants/index.js";
+import { words } from "../constants/index.js";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
-import {useGSAP} from '@gsap/react'
+import { useGSAP } from '@gsap/react'
 import gsap from "gsap";
 import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
@@ -24,7 +24,7 @@ const Hero = () => {
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
-                <img src="/images/bg.png" alt="background"/>
+                <img src="/images/bg.png" alt="background" />
             </div>
 
             <div className="hero-layout">
@@ -34,19 +34,19 @@ const Hero = () => {
                         <div className={"hero-text"}>
                             <h1>Shaping
                                 <span className="slide">
-                                <span className="wrapper">
-                                    {words.map((word) => (
-                                        <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                                            <img src={word.imgPath}
-                                                 alt={word.text}
-                                                 className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
-                                            />
+                                    <span className="wrapper">
+                                        {words.map((word) => (
+                                            <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                                <img src={word.imgPath}
+                                                    alt={word.text}
+                                                    className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                                />
 
-                                            <span>{word.text}</span>
-                                        </span>
-                                    ))}
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
                                 </span>
-                            </span>
                             </h1>
                             <h1>into Real Projects</h1>
                             <h1>that Deliver Results</h1>
@@ -56,19 +56,23 @@ const Hero = () => {
                             Hi, I'am Abin, a developer based in India with a passion for coding and science.
                         </p>
 
-                        <Button className={"md:w-80 md:h-16 w-60 h-12"} id="button" text={"See My Works"}/>
+                        <Button className={"md:w-80 md:h-16 w-60 h-12"} id="button" text={"See My Works"} />
+                        <a style={{ color: "white", fontSize: "20px" }} download href="/resume/Abin_Joseph_Resume.pdf" >
+                            Download Resume
+                        </a>
+
                     </div>
                 </header>
 
                 {/*3d Model*/}
                 <figure>
                     <div className={"hero-3d-layout"}>
-                        <HeroExperience/>
+                        <HeroExperience />
                     </div>
                 </figure>
             </div>
 
-            <AnimatedCounter/>
+            <AnimatedCounter />
         </section>
     )
 }
